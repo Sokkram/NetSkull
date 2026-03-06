8040199bd4505a9e5aed927c25f836d87bb7194f
Sokkram <dufauq123@gmail.com>
2026-03-05 20:11:29 -0700

    Created the first port scanner prototype

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#define SIZE 64
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,12 +10,25 @@
 
 
 int main(int argc, char *argv[]){
-    if(argc < 2) return 1;
-    
+    if(argc != 2) return 1;
+    int sock;
+    char buffer[SIZE];
     struct sockaddr_in adr;
     adr.sin_family = AF_INET;
-    adr.sin_port = htons(0); // It isn't finished yet 
     int ver = inet_aton(argv[1], &(adr.sin_addr));
 
     if(ver != 1) return 1;
+    for (unsigned short i=1; i<65535; i++){
+        sock = socket(PF_INET,SOCK_STREAM,0);
+        adr.sin_port = htons(i);
+        if(connect(sock,(struct sockaddr *)&adr, sizeof(struct sockaddr_in))==0)
+            printf("Open TCP port number: %hu\n",i);
+        else{
+            sock = socket(PF_INET, SOCK_DGRAM,0);
+            if(sendto(sock,buffer, strlen(buffer),32,(struct sockaddr *)&adr, sizeof(struct sockaddr_in))>=0)
+                printf("Open UDP port number: %hu\n",i);
+        }
+        close(sock);
+            
+    }
 }
